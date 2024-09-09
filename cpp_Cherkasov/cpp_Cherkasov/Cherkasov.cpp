10e365b0950401744a94285579778e67f73e0d2a
X3rL1x <nikitkacherkas836350@gmail.com>
2024-09-09 21:08:38 +0300

    Update Cherkasov.cpp

--- cpp_Cherkasov/cpp_Cherkasov/Cherkasov.cpp
+++ cpp_Cherkasov/cpp_Cherkasov/Cherkasov.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-	std::cout << "Hello world" << std::endl;
+	int count;
+	cout << "Hello world" << endl;
+	cout << "Entry some number" << endl;
+	cin >> count;
+	cout << count << endl;
+	cin.get();
 	return 0;
 }
