f48f01c00145008277f01a0aa3601a2a576f2a7b
X3rL1x <nikitkacherkas836350@gmail.com>
2024-09-10 12:10:34 +0300

    Update Cherkasov.cpp

--- cpp_Cherkasov/cpp_Cherkasov/Cherkasov.cpp
+++ cpp_Cherkasov/cpp_Cherkasov/Cherkasov.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-	int count, count1, count2;
+	int count, count1;
 	cout << "Hello world" << endl;
 	cout << "Entry some number" << endl;
 	cin >> count;
@@ -12,9 +12,6 @@ int main()
 	cout << "Entry some number" << endl;
 	cin >> count1;
 	cout << count1 << endl;
-	cout << "Entry some number" << endl;
-	cin >> count2;
-	cout << count2 << endl;
 	cin.get();
 	return 0;
 }
